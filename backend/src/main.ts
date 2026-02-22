9706869f5f8a273bcad451713ec8f8bdad2cd0f3
Jhorie Ranario <jmranario@student.apc.edu.ph>
2026-02-22 13:39:53 +0000

    update CORS configuration and adjust Vercel deployment settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: '*',
+     origin: [
+      'https://personal-website-finals-qnk1-bcc4z2rv5-jhories-projects.vercel.app',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
   
   app.useGlobalPipes(new ValidationPipe());
